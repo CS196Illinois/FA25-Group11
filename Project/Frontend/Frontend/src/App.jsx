a3386500cdbbac75baec988409a9b52c8ab48f83
Bwugg123 <benwuggazer@gmail.com>
2025-10-28 15:26:32 -0500

    Major / Minor Scene Commit

--- Project/Frontend/Frontend/src/App.jsx
+++ Project/Frontend/Frontend/src/App.jsx
@@ -8,27 +8,49 @@ import React, { useState } from "react";
 
 function App() {
   const [screen, setScreen] = useState('home');
-  const b1 = false;
-  const b2 = false;
-  const b3 = false;
-  const b4 = false;
-  const b5 = false;
-  const b6 = false;
-  const b7 = false;
-  const b8 = false;
-  const b9 = false;
-  const b10 = false;
-  const b11 = false;
-  const b12 = false;
+  const map = {};
+  const college = {};
 
   return (
     <div>
+      {screen === 'Majors' && (
+        <div>
+        <Option4 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "BUS"} />
+        <Option5 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "LAS"} />
+        <Option6 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "LAW"} />
+        <Option4 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "CIMED"} />
+        <Option5 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "AHS"} />
+        <Option6 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "ACES"} />
+        <Option4 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "EDUC"} />
+        <Option5 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "MEDIA"} />
+        <Option6 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "GCOE"} />
+        <Option4 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "SSW"} />
+        <Option5 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "iSchool"} />
+        <Option6 onClick={() => setScreen('Dropdown Menu')} handleClick = {() => college == "LER"} />
+        </div>
+      )}
+
+      {screen === 'Minors' && (
+        <div>
+        <Option4 onClick={() => setScreen('Dropdown Menu')} />
+        <Option5 onClick={() => setScreen('Dropdown Menu')} />
+        <Option6 onClick={() => setScreen('Dropdown Menu')} />
+        </div>
+      )}
+
+      {screen === 'Dropdown Menu' && (
+        <div>
+          
+          
+        </div>
+      )}
+
       {screen === 'home' && (
         <div>
           <h1><center> <b>Primary interest? </b></center></h1>
-          <Option1 onClick={() => setScreen('screen1')} handleClick = {() => b1 == true}/>
-          <Option2 onClick={() => setScreen('screen1')} handleClick = {() => b2 == true}/>
-          <Option3 onClick={() => setScreen('screen1')} handleClick = {() => b3 == true}/>
+          <Option1 onClick={() => setScreen('screen1')} handleClick = {() => map[1] == 1}/>
+          <Option2 onClick={() => setScreen('screen1')} handleClick = {() => map[1] == 2}/>
+          <Option3 onClick={() => setScreen('screen1')} handleClick = {() => map[1] == 3}/>
           
         </div>
       )}
@@ -36,9 +58,9 @@ function App() {
 
       {screen === 'screen1' && (
         <div>
-          <Option4 onClick={() => setScreen('screen2')} handleClick = {() => b4 == true} />
-          <Option5 onClick={() => setScreen('screen2')} handleClick ={() => b5 == true} />
-          <Option6 onClick={() => setScreen('screen2')} handleClick = {() => b6 == true} />
+          <Option4 onClick={() => setScreen('screen2')} handleClick = {() => map[2] == 1} />
+          <Option5 onClick={() => setScreen('screen2')} handleClick ={() => map[2] == 2} />
+          <Option6 onClick={() => setScreen('screen2')} handleClick = {() => map[2] == 3} />
         </div>
       )}
 
@@ -46,9 +68,9 @@ function App() {
 
       {screen === 'screen2' && (
         <div>
-        <Option4 onClick={() => setScreen('screen3')} handleClick = {() => b7 == true} />
-        <Option5 onClick={() => setScreen('screen3')} handleClick ={() => b8 == true} />
-        <Option6 onClick={() => setScreen('screen3')} handleClick = {() => b9 == true} />
+        <Option4 onClick={() => setScreen('screen3')} handleClick = {() => map[3] == 1} />
+        <Option5 onClick={() => setScreen('screen3')} handleClick ={() => map[3] == 2} />
+        <Option6 onClick={() => setScreen('screen3')} handleClick = {() => map[3] == 3} />
           
         </div>
       )}
@@ -57,9 +79,9 @@ function App() {
 
       {screen === 'screen3' && (
         <div>
-          <Option4 onClick={() => setScreen('tinder')} handleClick = {() => b10 == true} />
-          <Option5 onClick={() => setScreen('tinder')} handleClick ={() => b11 == true} />
-          <Option6 onClick={() => setScreen('tinder')} handleClick = {() => b12 == true} />
+          <Option4 onClick={() => setScreen('tinder')} handleClick = {() => map[4] == 1} />
+          <Option5 onClick={() => setScreen('tinder')} handleClick ={() => map[4] == 2} />
+          <Option6 onClick={() => setScreen('tinder')} handleClick = {() => map[4] == 3} />
         </div>
       )}
     
